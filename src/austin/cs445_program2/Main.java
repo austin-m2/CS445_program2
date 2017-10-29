1269c09ca4d9f6eda20dfce350f301967f30625d
austin-m2 <morris.austinm@gmail.com>
2017-10-28 23:22:31 -0700

    Started fill algorithm

--- src/austin/cs445_program2/Main.java
+++ src/austin/cs445_program2/Main.java
@@ -7,6 +7,8 @@ package austin.cs445_program2;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
@@ -42,7 +44,6 @@ public class Main {
             tempMatrix[2][1] = IDENTITY_MATRIX[2][1];
             tempMatrix[2][2] = IDENTITY_MATRIX[2][2];
             
-            
             switch(chars[0]) {
                 case "P":
                     Polygon newPolygon = new Polygon();
@@ -57,10 +58,10 @@ public class Main {
                     lastPolygon.transform = multiply(tempMatrix, lastPolygon.transform);
                     break;
                 case "r":
-                    tempMatrix[0][0] = (float) Math.cos(Math.toRadians(Double.parseDouble(chars[1])));
+                    tempMatrix[0][0] = (float)  Math.cos(Math.toRadians(Double.parseDouble(chars[1])));
                     tempMatrix[0][1] = (float) -Math.sin(Math.toRadians(Double.parseDouble(chars[1])));
-                    tempMatrix[1][0] = (float) Math.sin(Math.toRadians(Double.parseDouble(chars[1])));
-                    tempMatrix[1][1] = (float) Math.cos(Math.toRadians(Double.parseDouble(chars[1])));
+                    tempMatrix[1][0] = (float)  Math.sin(Math.toRadians(Double.parseDouble(chars[1])));
+                    tempMatrix[1][1] = (float)  Math.cos(Math.toRadians(Double.parseDouble(chars[1])));
                     lastPolygon.transform = multiply(tempMatrix, lastPolygon.transform);
                     break;
                 case "s":
@@ -132,20 +133,18 @@ public class Main {
                 glColor3f(1.0f, 1.0f, 0.0f);
                 glPointSize(1);
                 
-                for (int i = 0; i < polygons.size(); i++) {
-                    Polygon temp = (Polygon) polygons.get(i);
-                    glColor3f(temp.color[0], temp.color[1], temp.color[2]);
-                    glBegin(GL_POLYGON);
-                        for (int j = 0; j < temp.vertices.size(); j++) {
-                            float[] vertex = (float[]) temp.vertices.get(j);
-                            glVertex2f(vertex[0], vertex[1]);
-                        }
-                    glEnd();
-                }
+                drawPolygon((Polygon) polygons.get(0));
                 
-                glBegin(GL_POLYGON);
-                    
-                glEnd();
+//                for (int i = 0; i < polygons.size(); i++) {
+//                    Polygon temp = (Polygon) polygons.get(i);
+//                    glColor3f(temp.color[0], temp.color[1], temp.color[2]);
+//                    glBegin(GL_POLYGON);
+//                        for (int j = 0; j < temp.vertices.size(); j++) {
+//                            float[] vertex = (float[]) temp.vertices.get(j);
+//                            glVertex2f(vertex[0], vertex[1]);
+//                        }
+//                    glEnd();
+//                }
                 
                 Display.update();
                 Display.sync(60);
@@ -168,4 +167,64 @@ public class Main {
         }
         return product;
     }
+    
+    
+    public static void drawPolygon(Polygon polygon) {
+        ArrayList<float[]> vertices = polygon.vertices;
+        ArrayList<float[]> all_edges = new ArrayList();
+        ArrayList<float[]> global_edges = new ArrayList();
+        ArrayList<float[]> active_edges = new ArrayList();
+        
+        
+        //populate all_edges list
+        for (int i = 0; i < vertices.size(); i++) {
+            float[] currVertex = vertices.get(i);
+            float[] nextVertex = vertices.get((i + 1) % vertices.size());
+            float x0 = currVertex[0];
+            float y0 = currVertex[1];
+            float x1 = nextVertex[0];
+            float y1 = nextVertex[1];
+            
+            float[] edge = new float[4];
+            edge[0] = (y0 <= y1) ? y0 : y1; //y-min
+            edge[1] = (y0 >= y1) ? y0 : y1; //y-max
+            edge[2] = (y0 <= y1) ? x0 : x1; //x-val
+            edge[3] = (x1 - x0) / (y1 - y0); //1/m
+            all_edges.add(edge);
+        }
+        
+        //move edges from all_edges into global_edges
+        for (int i = 0; i < all_edges.size(); i++) {
+            float[] edge = all_edges.get(i);
+            if (edge[3] == Float.NaN || edge[3] == Float.POSITIVE_INFINITY || edge[3] == Float.NEGATIVE_INFINITY) {
+                continue;
+            }
+            global_edges.add(edge);
+        }
+        
+
+        Collections.sort(global_edges, new Comparator<float[]>() {
+            @Override
+            public int compare(float[] edge1, float[] edge2) {
+                if (edge1[0] < edge2[0]) return -1;
+                if (edge1[0] > edge2[0]) return 1;
+
+                //y-min is equal
+                if (edge1[2] < edge2[2]) return -1;
+                if (edge1[2] > edge2[2]) return 1;
+
+                //x-val is equal
+                if (edge1[1] < edge2[1]) return -1;
+                if (edge1[1] > edge2[1]) return 1;
+
+                //y-max is equal
+                return 0;
+            }
+        });
+
+        
+        
+    }
 }
+
+
